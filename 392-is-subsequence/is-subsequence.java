975a685b741dd215507bd9d0d075ac9450deac14
saurabhhajela14 <hajelasaurabh1@gmail.com>
2026-08-20 22:46:05 +0530

    Time: 2 ms (73.55%) | Memory: 42.8 MB (63.38%) - LeetSync

---
+++ 392-is-subsequence/is-subsequence.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int i=0;
+        if(s.length()==0) {
+            return true;
+        }
+        for(int j=0;j<t.length();j++) {
+            if(i<s.length() && s.charAt(i)==t.charAt(j)) {
+                i++;
+            if(i==s.length()) {
+                return true;
+            }
+        }
+        }
+            return false;
+            }
+    }
